4de3c57c33095d174cbab93b5218da962c942323
Metin <durmus38metin@gmail.com>
2023-06-17 20:48:30 +0300

    emacs init.el

---
+++ init.el
@@ -0,0 +1,45 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(setq make-backup-files nil)
+
+(delete-selection-mode 1)
+(global-unset-key (kbd "C-z"))
+(global-set-key (kbd "C-z")   'undo-fu-only-undo)
+(global-set-key (kbd "C-S-z") 'undo-fu-only-redo)
+(setq select-enable-clipboard t)
+(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+
+
+(require 'ido)
+;;(ido-mode t)
+(global-set-key (kbd "M-x") 'smex)
+(ido-everywhere t)
+(flyspell-mode t)
+(setq flyspell-prog-mode t)
+(flymake-mode t)
+(global-flycheck-mode t)
+(global-display-line-numbers-mode t)
+
+(global-set-key (kbd "C-S-c") 'kill-ring-save)
+(global-set-key (kbd "C-v") 'yank)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "ba4ab079778624e2eadbdc5d9345e6ada531dc3febeb24d257e6d31d5ed02577" default))
+ '(package-selected-packages
+   '(astyle format-all clang-format flycheck zenburn-theme undo-fu smex gruber-darker-theme flymake flx-ido company-c-headers chess avy auto-complete-clang)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;;a
